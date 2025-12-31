14c4e4fd682529bdd02ff523a5ba7f8a5711c952
Alex <153161367+DjAlex2017@users.noreply.github.com>
2025-12-30 22:16:08 -0700

    Remove hardcoded Google Maps API key

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -8,7 +8,7 @@ import GoogleMaps
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    GMSServices.provideAPIKey("AIzaSyCz_BTLnfzy-6dNAPltswztJD1OAjbs3F8")  
+    GMSServices.provideAPIKey("YOUR_GOOGLE_MAPS_API_KEY")  
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
